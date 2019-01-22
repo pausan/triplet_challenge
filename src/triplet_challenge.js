2d21d476c4f588163b3c6839eedeaeb854559775
Pau Sanchez <contact@pausanchez.com>
2019-01-22 20:33:22 +0100

    Implementing custom hash table, that does not work yet.

--- src/triplet_challenge.js
+++ src/triplet_challenge.js
@@ -2,10 +2,75 @@ const fs = require('fs');
 
 const MAX_WORD_SIZE = 1024;
 
+const HASH_TABLE_PRIMES = [
+  389, 769, 1543, 3079, 6151, 12289, 24593, 49157, 98317, 196613,
+  786433, 1572869, 3145739, 6291469, 12582917, 50331653, 100663319,
+  201326611
+];
+
+// -----------------------------------------------------------------------------
+// HashTableCounter
+//
+// Custom hash table
+// -----------------------------------------------------------------------------
+class HashTableCounter {
+  constructor (size) {
+    let optimalSize = 1.25*size;
+    for (let i = 0; i < HASH_TABLE_PRIMES.length; i++) {
+      if (HASH_TABLE_PRIMES[i] > optimalSize) {
+        optimalSize = HASH_TABLE_PRIMES[i]
+        break;
+      }
+    }
+
+    this.counters = new Uint32Array(optimalSize);
+    this.triplets = new Array(optimalSize);
+    this.hashTableSize = optimalSize;
+  }
+
+  fnv(word, result) {
+    const PRIME = 16777619;
+    for (let i = 0; i < word.length; i++) {
+      result ^= word[i];
+      result *= PRIME;
+    }
+
+    return (result < 0 ? -result : result);
+  }
+
+  addTriplet (triplet) {
+    const hash2 = this.fnv(triplet[2], 0);
+    const hash1 = this.fnv(triplet[1], hash2);
+    const hash0 = this.fnv(triplet[1], hash1);
+    const offset = hash0 % this.hashTableSize;
+
+    while (
+      this.triplets[offset] !== undefined
+      && !this.tripletEquals (this.triplets[offset], triplet)
+    ) {
+      offset++;
+      if (offset > this.hashTableSize) {
+        offset = 0;
+      }
+    }
+    this.triplets [offset] = triplet;
+    this.counters [ offset ] ++;
+  }
+
+  tripletEquals (a, b) {
+    for (let i = 0; i < 3; i++) {
+      if (a[i] != b[i])
+        return false;
+    }
+
+    return true;
+  }
+};
+
 // -----------------------------------------------------------------------------
 // getSanitizedWordsGenerator
 //
-// Returns a generator with all the words from the file once sanitized
+// Returns all words from the file once sanitized
 // -----------------------------------------------------------------------------
 function *getSanitizedWordsGenerator (raw) {
   const char_a = 'a'.charCodeAt(0);
@@ -19,6 +84,7 @@ function *getSanitizedWordsGenerator (raw) {
 
   const sanitizedWord = Buffer.alloc(MAX_WORD_SIZE, ' ', 'ascii');
 
+  let words = [];
   let lastChar = 0;
   for (let i = 0, j = 0; i < raw.length; i++) {
     let char = raw[i];
@@ -61,13 +127,14 @@ function readSanitizedWords (file) {
 function *getTripletsGenerator (allWordsGenerator) {
   const triplet = [];
   for (const wordBytes of allWordsGenerator) {
-    triplet.push (wordBytes.toString());
+    triplet.push (new Buffer(wordBytes));
     if (triplet.length == 3) {
-      yield triplet.join(' ');
+      yield triplet;
       triplet.shift();
     }
   }
 }
+
 
 // -----------------------------------------------------------------------------
 // findTriplets
@@ -76,20 +143,23 @@ function *getTripletsGenerator (allWordsGenerator) {
 // -----------------------------------------------------------------------------
 function findTriplets (allWordsGenerator) {
   const tripletsByWordLength = new Array(MAX_WORD_SIZE);
-
   const tripletsCount = {};
+
+  const tripletsCounter = new HashTableCounter(50000); // FIXME!
+
   for (const triplet of getTripletsGenerator (allWordsGenerator)) {
-    if (triplet in tripletsCount)
-      tripletsCount[triplet] ++;
-    else
-      tripletsCount[triplet] = 1;
+    tripletsCounter.addTriplet (triplet);
   }
 
   // Print triplets with highest count
   let top1 = { c: 0, v : ''};
   let top2 = { c: 0, v : ''};
   let top3 = { c: 0, v : ''};
-  for (var [triplet, count] of Object.entries(tripletsCount)) {
+
+  for (let i = 0; i < tripletsCounter.hashTableSize; i++) {
+    const triplet = tripletsCounter.triplets[i];
+    const count = tripletsCounter.counters[i];
+
     if (count >= top1.c)  {
       top3 = top2;
       top2 = top1;
@@ -106,9 +176,9 @@ function findTriplets (allWordsGenerator) {
     }
   }
 
-  console.log (top1.v, '-', top1.c);
-  console.log (top2.v, '-', top2.c);
-  console.log (top3.v, '-', top3.c);
+  console.log (top1.v.join (' '), '-', top1.c);
+  console.log (top2.v.join (' '), '-', top2.c);
+  console.log (top3.v.join (' '), '-', top3.c);
 }
 
 
@@ -123,8 +193,8 @@ function main () {
 
   fileToProcess = process.argv[2];
 
-  const allWordsGenerator = readSanitizedWords (fileToProcess);
-  findTriplets (allWordsGenerator);
+  const allWords = readSanitizedWords (fileToProcess);
+  findTriplets (allWords);
 }
 
 
