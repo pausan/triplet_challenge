c672bdbb65db5c9fbb26a8784c0bb096740e5146
Pau Sanchez <contact@pausanchez.com>
2019-01-23 19:47:58 +0100

    Implement bonding algorithm from C version

--- src/triplet_challenge.js
+++ src/triplet_challenge.js
@@ -41,7 +41,7 @@ function *allTripletsGenerator (raw) {
 
         j++;
         if (w3start > 0)
-          yield tripletBytes.toString();
+          yield tripletBytes;
 
         w1start = w2start;
         w2start = w3start;
@@ -52,16 +52,48 @@ function *allTripletsGenerator (raw) {
 }
 
 // -----------------------------------------------------------------------------
-// findTriplets
+// fnv
 //
-// Counts all triplets
+// compute hash on given byteBuffer
 // -----------------------------------------------------------------------------
-function findTriplets (file) {
-  const tripletsByWordLength = new Array(MAX_WORD_SIZE);
-  const triplets = allTripletsGenerator (fs.readFileSync (file));
+function fnv(byteBuffer) {
+  const PRIME = 16777619;
+  let result = 0;
+  for (let i = 0; i < byteBuffer.length; i++) {
+    result ^= byteBuffer[i];
+    result *= PRIME;
+  }
+  return (result < 0 ? -result : result);
+}
+
+// -----------------------------------------------------------------------------
+// findHighestCountTripletsForArray
+// -----------------------------------------------------------------------------
+function findHighestCountTripletsForArray (triplets, countThreshold) {
+  // first find all items that have collisions
+  const collisionCounter = new Uint32Array(triplets.length / 8);
+  const collisionLen = collisionCounter.length;
+
+  for (let i = 0; i < triplets.length; i++) {
+    const hash = fnv (triplets[i]) % collisionLen;
+    collisionCounter[hash] ++;
+  }
+
+  let tripletsSubset = [];
+
+  // now recompute again, but remove all those items that have collided less than
+  // countThreshold
+  for (let i = 0; i < triplets.length; i++) {
+    const hash = fnv (triplets[i]) % collisionLen;
+    if (collisionCounter[hash] >= countThreshold) {
+      tripletsSubset.push (triplets[i].toString());
+    }
+  }
 
   const tripletsCount = new Map();
-  for (const triplet of triplets) {
+
+  for (let i = 0; i < tripletsSubset.length; i++) {
+    const triplet = tripletsSubset[i];
     if (!tripletsCount.has (triplet)) {
       tripletsCount.set (triplet, 1);
     }
@@ -91,9 +123,75 @@ function findTriplets (file) {
     }
   }
 
-  console.log (top1.v, '-', top1.c);
-  console.log (top2.v, '-', top2.c);
-  console.log (top3.v, '-', top3.c);
+  return [
+    { v : top1.v, c : top1.c },
+    { v : top2.v, c : top2.c },
+    { v : top3.v, c : top3.c }
+  ];
+}
+
+// -----------------------------------------------------------------------------
+// mergeTriplets
+// -----------------------------------------------------------------------------
+function mergeTriplets (winningTriplet, partialTriplet) {
+  for (let i = 0; i < partialTriplet.length; i++) {
+    const count = partialTriplet[i].c;
+    const triplet = partialTriplet[i].v;
+
+    if (count >= winningTriplet[0].c)  {
+      winningTriplet[2] = winningTriplet[1];
+      winningTriplet[1] = winningTriplet[0];
+      winningTriplet[0] = { c: count, v : triplet};
+    }
+
+    else if (count >= winningTriplet[1].c)  {
+      winningTriplet[2] = winningTriplet[1];
+      winningTriplet[1] = { c: count, v : triplet};
+    }
+
+    else if (count >= winningTriplet[2].c)  {
+      winningTriplet[2] = { c: count, v : triplet};
+    }
+  }
+
+  return winningTriplet;
+}
+
+// -----------------------------------------------------------------------------
+// findTriplets
+//
+// Counts all triplets
+// -----------------------------------------------------------------------------
+function findTriplets (file) {
+  const tripletsByWordLength = new Array(MAX_WORD_SIZE);
+  const triplets = allTripletsGenerator (fs.readFileSync (file));
+
+  // initialize all lengths with an empty array
+  for (let i = 0; i < tripletsByWordLength.length; i++)
+    tripletsByWordLength[i] = [];
+
+  // insert each triplet to the bucket with their length
+  for (const triplet of triplets) {
+    tripletsByWordLength[triplet.length].push (triplet);
+  }
+
+  // solve the problem for a subarray
+  let winningTriplet = [ { v : '', c: 0} , { v : '', c : 0}, { v : '', c: 0} ];
+
+  for (let i = 0; i < tripletsByWordLength.length; i++) {
+    if (tripletsByWordLength[i].length == 0)
+      continue;
+
+    let countThreshold = winningTriplet[2].c;
+    let partialTriplet = findHighestCountTripletsForArray (tripletsByWordLength[i], countThreshold);
+
+    // merge triplets
+    winningTriplet = mergeTriplets (winningTriplet, partialTriplet);
+  }
+
+  console.log (winningTriplet[0].v.toString(), '-', winningTriplet[0].c);
+  console.log (winningTriplet[1].v.toString(), '-', winningTriplet[1].c);
+  console.log (winningTriplet[2].v.toString(), '-', winningTriplet[2].c);
 }
 
 // -----------------------------------------------------------------------------
@@ -115,3 +213,9 @@ function main () {
 // Entry point
 // -----------------------------------------------------------------------------
 main ();
+/*a = Buffer.from([1, 2, 3, 4, 5, 6])
+b = [];
+
+b.push (a.slice(1, 3))
+b.push (a.slice(2, 4))
+console.log (b);*/
