b426097d249310d26e34d517c2ce892faa1cbb69
Pau Sanchez <contact@pausanchez.com>
2019-01-24 09:23:02 +0100

    Implementing bounding algorithm in golang

--- src/triplet_challenge.go
+++ src/triplet_challenge.go
@@ -6,6 +6,11 @@ import (
   "io/ioutil"
 )
 
+type Triplet struct {
+  triplet  string
+  count    int
+}
+
 // -----------------------------------------------------------------------------
 // sanitizeData
 // -----------------------------------------------------------------------------
@@ -38,32 +43,9 @@ func sanitizeData (data []byte) []byte {
 }
 
 // -----------------------------------------------------------------------------
-// countTriplets
+// findWinningTriplets
 // -----------------------------------------------------------------------------
-func countTriplets (sanitized []byte) map[string]int {
-  var counter map[ string ]int = make(map[string]int)
-
-  off1, off2, off3 := 0, 0, 0
-  for off:= 0; off < len(sanitized); off++ {
-    if (sanitized[off] != 32) {
-      continue
-    }
-
-    if off2 != 0 {
-      triplet := string(sanitized[off3 : off])
-      counter[triplet]++
-    }
-
-    off3 = off2
-    off2 = off1
-    off1 = off + 1
-  }
-
-  return counter
-}
-
-
-func printHighestThree (counter map[string]int) {
+func findWinningTriplets (counter map[string]int) []Triplet {
   t1, t2, t3 := "", "", ""
   c1, c2, c3 :=  0,  0,  0
 
@@ -89,10 +71,110 @@ func printHighestThree (counter map[string]int) {
     }
   }
 
-  fmt.Printf ("%s - %d\n", t1, int(c1))
-  fmt.Printf ("%s - %d\n", t2, int(c2))
-  fmt.Printf ("%s - %d\n", t3, int(c3))
+  return []Triplet {
+    Triplet{t1, c1},
+    Triplet{t2, c2},
+    Triplet{t3, c3},
+  }
 }
+
+// -----------------------------------------------------------------------------
+// fnvHash32
+// Compute a string hash
+// -----------------------------------------------------------------------------
+func fnvHash32(data []byte) uint32 {
+  const PRIME = 16777619
+  var result uint32 = 0
+
+  // continue iterating through the rest of the string
+  for i := 0; i < len(data); i++ {
+    result ^= uint32(data[i])
+    result *= PRIME
+  }
+
+  return result
+}
+
+// -----------------------------------------------------------------------------
+// findHighestTripletsWithBounding
+// -----------------------------------------------------------------------------
+func findHighestTripletsWithBounding(tripletsOfLenghtX [][]byte, threshold int) []Triplet {
+  var counter map[ string ]int = make(map[string]int)
+
+  var hashes []uint32 = make ([]uint32, len(tripletsOfLenghtX))
+  var collisionMap []int = make ([]int, 10 + len(tripletsOfLenghtX) / 8)
+  var collisionMapLen uint32 = uint32(len(collisionMap))
+
+  for i := 0; i < len(tripletsOfLenghtX); i++ {
+    hash := fnvHash32 (tripletsOfLenghtX[i]) % collisionMapLen
+    collisionMap[hash] ++
+    hashes[i] = hash
+  }
+
+  newLen := 0
+  for i := 0; i < len(tripletsOfLenghtX); i++ {
+    if collisionMap [hashes[i]] <= threshold {
+      continue
+    }
+
+    tripletsOfLenghtX[newLen] = tripletsOfLenghtX[i]
+    newLen++
+  }
+
+  tripletsOfLenghtX = tripletsOfLenghtX[0:newLen]
+
+  for i := 0; i < len(tripletsOfLenghtX); i++ {
+    triplet := string(tripletsOfLenghtX[i])
+    counter [triplet]++
+  }
+
+  return findWinningTriplets(counter)
+}
+
+// -----------------------------------------------------------------------------
+// mergeTriplets
+// -----------------------------------------------------------------------------
+func mergeTriplets (winning []Triplet, partial []Triplet) {
+  for i := 0; i < len(winning); i++ {
+    if partial[i].count > winning[0].count {
+      winning[1] = winning[0]
+      winning[2] = winning[1]
+      winning[0] = partial[i]
+    } else if partial[i].count > winning[1].count {
+      winning[2] = winning[1]
+      winning[1] = partial[i]
+    } else if partial[i].count > winning[2].count {
+      winning[2] = partial[i]
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// getTripletsPerLength
+// -----------------------------------------------------------------------------
+func getTripletsPerLength (sanitized []byte) [][][]byte {
+
+  var tripletsPerLength [][][]byte = make([][][]byte, 128)
+
+  off1, off2, off3 := 0, 0, 0
+  for off:= 0; off < len(sanitized); off++ {
+    if (sanitized[off] != 32) {
+      continue
+    }
+
+    if off2 != 0 {
+      triplet := sanitized[off3 : off]
+      tripletsPerLength[len(triplet)] = append (tripletsPerLength[len(triplet)], triplet)
+    }
+
+    off3 = off2
+    off2 = off1
+    off1 = off + 1
+  }
+
+  return tripletsPerLength
+}
+
 
 // -----------------------------------------------------------------------------
 // findTriplets
@@ -100,8 +182,28 @@ func printHighestThree (counter map[string]int) {
 func findTriplets (data []byte) {
   var sanitized []byte = sanitizeData (data)
 
-  counter := countTriplets(sanitized)
-  printHighestThree (counter)
+  tripletsPerLength := getTripletsPerLength(sanitized)
+
+  // iterate on each bucket of size X
+  var winningTriplets = []Triplet {
+    Triplet{"", 0},
+    Triplet{"", 0},
+    Triplet{"", 0},
+  }
+
+  for i := 0; i < len(tripletsPerLength); i++ {
+    if len(tripletsPerLength[i]) == 0 {
+      continue
+    }
+
+    threshold := winningTriplets[2].count
+    partialTriplets := findHighestTripletsWithBounding (tripletsPerLength[i], threshold)
+    mergeTriplets(winningTriplets, partialTriplets)
+  }
+
+  for i := 0; i < len(winningTriplets); i++ {
+    fmt.Printf ("%s - %d\n", winningTriplets[i].triplet, winningTriplets[i].count)
+  }
 }
 
 // -----------------------------------------------------------------------------
